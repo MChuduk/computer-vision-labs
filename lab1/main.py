74edebc732b14237810edf830047deb7036e439a
MChuduk <mihail2014mihail@gmail.com>
2022-09-07 10:05:51 +0300

    Fix tasks

--- lab1/main.py
+++ lab1/main.py
@@ -2,8 +2,40 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
-def to_grey(image):
-    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def task1():
+    image = cv2.imread('1.jpg');
+    cv2.imshow('image', image);
+    cv2.waitKey(0);
+    return image;
+
+def task2():
+    image = cv2.imread('1.jpg', cv2.IMREAD_GRAYSCALE);
+    cv2.imshow('image', image);
+    cv2.waitKey(0);
+    return image;
+
+def task3():
+    image = cv2.imread('1.jpg', cv2.IMREAD_GRAYSCALE);
+    thresh = 128;
+    image = cv2.threshold(image, thresh, 255, cv2.THRESH_BINARY)[1];
+    cv2.imshow('image', image);
+    cv2.waitKey(0);
+    return image;
+
+def task4(filename, image):
+    cv2.imwrite(filename, image);
+
+def task5():
+    image = cv2.imread('2.jpg');
+    show_hists(image, "hist_before.png");
+    R, G, B = cv2.split(image)
+    output1_R = cv2.equalizeHist(R)
+    output1_G = cv2.equalizeHist(G)
+    output1_B = cv2.equalizeHist(B)
+    image = cv2.merge((output1_R, output1_G, output1_B))
+    cv2.imshow("image", image);
+    cv2.waitKey(0);
+    show_hists(image, "hist_after.png");
 
 def compute_hists(image):
     hist_b = cv2.calcHist([image], channels=[0], mask=None, histSize=[256], ranges=[0, 256])
@@ -21,31 +53,5 @@ def show_hists(rgb_image, name="hist.png"):
     plot = cv2.imread(name)
     cv2.imshow(name, plot)
     cv2.waitKey(0)
-    
-image = cv2.imread("1.jpg")
 
-# задание 1 - вывести изображение 
-cv2.imshow("image", image);
-cv2.waitKey(0);
-
-# задание 2 - изображение в оттеках серого
-# image = to_grey(image);
-# cv2.imshow("image", image);
-# cv2.waitKey(0);
-
-# задание 3 - бинарное изображение
-# image[:, :, 0] = 0
-# image[:, :, 2] = 0
-# cv2.imshow("image", image);
-# cv2.waitKey(0);
-
-# задание 4 - гистограммы 
-# show_hists(image, "hist_before.png");
-# R, G, B = cv2.split(image)
-# output1_R = cv2.equalizeHist(R)
-# output1_G = cv2.equalizeHist(G)
-# output1_B = cv2.equalizeHist(B)
-# image = cv2.merge((output1_R, output1_G, output1_B))
-# cv2.imshow("image", image);
-# cv2.waitKey(0);
-# show_hists(image, "hist_after.png");
+task5();
